1329ed15c4302972c2ef054e75f6f7402cc8e180
VLAD EMINOVICI <veminovici@hotmail.com>
2021-01-13 20:38:54 +0200

    Separate the common search from the specific one.

--- src/Boole/Stochastic/RandomSearch.fs
+++ src/Boole/Stochastic/RandomSearch.fs
@@ -3,6 +3,18 @@ namespace Simplee.Intelligence.Stochastic
 open Simplee
 open Simplee.State.ComputationExpression
 
+[<RequireQualifiedAccess>]
+module StochasticSearch = 
+
+    /// run this function each iteration.
+    let private iteration (newSln: State<'TState, 'TSln>) (cmpSln: 'TSln -> State<'TState, 'TSln>) = _state {
+        let! sln = newSln
+        let! bst = cmpSln sln
+        return bst }
+
+    let run newSln cmpSln = 
+        State.unfold (iteration newSln cmpSln)
+
 type RSState = {
     Best: int
     Current: int list }
@@ -21,10 +33,4 @@ module RandomSearch =
         | sln when sln > stt.Best && sln % 5 = 0 -> sln, {stt with Best = sln }
         | _                                      -> stt.Best, stt
 
-    /// run this function each iteration.
-    let iteration = _state {
-        let! sln = newSolution
-        let! bst = cmpSolution sln
-        return bst }
-
-    let run = State.unfold iteration
+    let run = StochasticSearch.run newSolution cmpSolution

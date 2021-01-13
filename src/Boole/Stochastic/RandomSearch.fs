dfc8cdda658a47ebedf65799e70ae77bdaf0a4c0
VLAD EMINOVICI <veminovici@hotmail.com>
2021-01-13 20:42:18 +0200

    Extract other non-generic behavior

--- src/Boole/Stochastic/RandomSearch.fs
+++ src/Boole/Stochastic/RandomSearch.fs
@@ -14,23 +14,3 @@ module StochasticSearch =
 
     let run newSln cmpSln = 
         State.unfold (iteration newSln cmpSln)
-
-type RSState = {
-    Best: int
-    Current: int list }
-
-[<RequireQualifiedAccess>]
-module RandomSearch = 
-
-    // We just increment a counter.
-    let private newSolution = State <| fun (stt: RSState) ->
-        let sln = List.length stt.Current
-        sln, {stt with Current = stt.Current @ [sln] }
-
-    // The best solution, is the biggest int that divides with 5.
-    let private cmpSolution sln = State <| fun (stt: RSState) ->
-        match sln with
-        | sln when sln > stt.Best && sln % 5 = 0 -> sln, {stt with Best = sln }
-        | _                                      -> stt.Best, stt
-
-    let run = StochasticSearch.run newSolution cmpSolution
--- tests/XUno/Stochastic/TRandomSearch.fs
+++ tests/XUno/Stochastic/TRandomSearch.fs
@@ -5,7 +5,28 @@ module TRandomSearch =
     open Xunit
     open Xunit.Abstractions
 
+    open Simplee
     open Simplee.Intelligence.Stochastic
+
+    type RSState = {
+        Best: int
+        Current: int list }
+
+    [<RequireQualifiedAccess>]
+    module RandomSearch = 
+
+        // We just increment a counter.
+        let private newSolution = State <| fun (stt: RSState) ->
+            let sln = List.length stt.Current
+            sln, {stt with Current = stt.Current @ [sln] }
+
+        // The best solution, is the biggest int that divides with 5.
+        let private cmpSolution sln = State <| fun (stt: RSState) ->
+            match sln with
+            | sln when sln > stt.Best && sln % 5 = 0 -> sln, {stt with Best = sln }
+            | _                                      -> stt.Best, stt
+
+        let run = StochasticSearch.run newSolution cmpSolution
 
     type Tests (output: ITestOutputHelper) =
 
